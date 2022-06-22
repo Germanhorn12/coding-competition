96902e23c228a6b172a14f19f444baa43b1b78d3
Germanhorn12 <108017639+Germanhorn12@users.noreply.github.com>
2022-06-22 18:37:23 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+scene.setBackgroundColor(7)
+let epicGuy = sprites.create(img`
+    . . f f f f f f f f f f f . . . 
+    . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+    . . f f f f f f f f f f f f . . 
+    . . f f f f f f f f f 1 f . . . 
+    . . f f f f f f f f 1 1 f . . . 
+    . . f f f f f f f f f f f . . . 
+    . . . f f f f f f f f f 1 . . . 
+    . . f f f 2 2 2 2 2 f f f . . . 
+    . . f 2 f 2 2 2 2 2 f 2 f . . . 
+    . . f 2 f 2 2 2 2 2 f 2 f . . . 
+    . . f f f 2 2 2 2 2 f f f . . . 
+    . . . . f f f f f f f . . . . . 
+    . . . . f 2 f . f 2 f . . . . . 
+    . . . . f 2 f . f 2 f . . . . . 
+    . . . . f 2 f . f 2 f . . . . . 
+    . . . . f f f . f f f . . . . . 
+    `, SpriteKind.Player)
